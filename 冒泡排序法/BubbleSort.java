39cc32f436d90eaa69f8417f0abc7e1021f9d2cf
梦影 <54546721+gxmandppx@users.noreply.github.com>
2021-06-05 13:19:40 +0800

    Update BubbleSort.java

--- 冒泡排序法/BubbleSort.java
+++ 冒泡排序法/BubbleSort.java
@@ -1,1 +1,30 @@
-
+import java.util.Scanner;
+public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("输入你想要比较数字的个数：");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("输入你想要比较数字：");
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++)
+        {
+            for(int j=0;j<arr.length-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        for(int j:arr)
+        {
+            System.out.print(j + "\t");
+        }
+    }
+}
